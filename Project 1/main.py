9e300147155867cc8a32bdceadcd6dfaf48b3656
Marc Cummings <marccummings@live.dk>
2023-09-12 12:28:28 +0200

    Started on project 1

---
+++ Project 1/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the data file line by line
+with open('Data/ecoli.data', 'r') as file:
+    lines = file.readlines()
+
+# Split each line into values and store them in a list of lists
+data = [line.split() for line in lines]
+
+# Create a DataFrame from the list of lists
+df = pd.DataFrame(data)
+
+# Convert columns 1 to 7 to numeric (assuming they are numeric data)
+df.iloc[:, 1:8] = df.iloc[:, 1:8].apply(pd.to_numeric)
+
+df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+
+print(df.head())
+print(df.shape)
