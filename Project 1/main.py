53be5def34b8a4c76841f8384392fcd818d2784b
kiwimarc <marccummings@live.dk>
2023-09-16 20:14:41 +0200

    Started on data analysis

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import svd
+
+
+#####################
+#                   #
+#    Data import    #
+#                   #
+#####################
 
 # Read the data file line by line
 with open('Data/ecoli.data', 'r') as file:
@@ -11,10 +20,64 @@ data = [line.split() for line in lines]
 # Create a DataFrame from the list of lists
 df = pd.DataFrame(data)
 
-# Convert columns 1 to 7 to numeric (assuming they are numeric data)
-df.iloc[:, 1:8] = df.iloc[:, 1:8].apply(pd.to_numeric)
-
 df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
 
 print(df.head())
-print(df.shape)
+
+cols = range(1,8)
+
+# Extract the attributes into a matrix
+X = np.asarray(df.values[:, cols])
+
+X = X.astype(float) 
+
+# Extract the attribute names that came from the header
+attributeNames = np.asarray(df.columns[cols])
+
+# Extracting the strings for each sample from the raw data loaded from the csv:
+classLabels = df.values[:,-1] # -1 takes the last column
+# Then determine which classes are in the data by finding the set of 
+# unique class labels 
+classNames = np.unique(classLabels)
+# Assign each type of the classes with a number by making a
+# Python dictionary
+classDict = dict(zip(classNames,range(len(classNames))))
+
+# Class index vector y
+y = np.array([classDict[cl] for cl in classLabels])
+
+# Determine the number of data objects and number of attributes
+N, M = X.shape
+
+# Number of classes
+C = len(classNames)
+
+#######################
+#                     #
+#    Data analysis    #
+#                     #
+#######################
+
+
+# Subtract mean value from data
+Y = X - np.ones((N,1))*X.mean(axis=0)
+
+# PCA by computing SVD of Y
+U,S,V = svd(X,full_matrices=False)
+
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum() 
+
+threshold = 0.9
+
+# Plot variance explained
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
