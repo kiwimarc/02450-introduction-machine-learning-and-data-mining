cf71f835159fa3a579df3a08671e60dee26073d7
Marc Cummings <marccummings@live.dk>
2023-10-22 10:43:51 +0200

    Added data import part from project 1 to project 2

---
+++ Project 2/main.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import svd
+
+# If set to true, then plots will be shown. Scripts stops after showing the plot, thats when
+# this variable may come in handy.
+SHOW_PLOTS = False
+
+
+#################################
+#                               #
+#          Project 1            #
+#                               #
+#################################
+
+
+#####################
+#                   #
+#    Data import    #
+#                   #
+#####################
+
+print("\n##### DATA IMPORT #####\n")
+
+# Read the data file line by line
+with open('Data/ecoli.data', 'r') as file:
+    lines = file.readlines()
+
+# Split each line into values and store them in a list of lists
+data = [line.split() for line in lines]
+
+# Create a DataFrame from the list of lists
+df = pd.DataFrame(data)
+
+df.columns = ['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2', 'class']
+
+print(df.head())
+
+cols = range(1,8)
+
+# Extract the attributes into a matrix
+X = np.asarray(df.values[:, cols])
+
+X = X.astype(float) 
+
+# Extract the attribute names that came from the header
+attributeNames = np.asarray(df.columns[cols])
+
+# Extracting the strings for each sample from the raw data loaded from the csv:
+classLabels = df.values[:,-1] # -1 takes the last column
+# Then determine which classes are in the data by finding the set of 
+# unique class labels 
+classNames = np.unique(classLabels)
+# Assign each type of the classes with a number by making a
+# Python dictionary
+classDict = dict(zip(classNames,range(len(classNames))))
+
+# Class index vector y
+y = np.array([classDict[cl] for cl in classLabels])
+
+# Determine the number of data objects and number of attributes
+N, M = X.shape
+
+# Number of classes
+C = len(classNames)
