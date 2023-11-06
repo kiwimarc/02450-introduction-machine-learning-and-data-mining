ca794c5d48e5935d064bea07f207a7f09afde9d4
kiwimarc <marccummings@live.dk>
2023-11-06 10:47:05 +0100

    Made multilinear regression

--- Project 2/classification.py
+++ Project 2/classification.py
@@ -1,8 +1,8 @@
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+import sklearn.linear_model as lm
 
 def run(X, df, classLabels, np, pd, plt):
     print("\n##### CLASSIFICATION #####\n")
@@ -10,7 +10,7 @@ def run(X, df, classLabels, np, pd, plt):
     data_train, data_test, label_train, label_test = train_test_split(X, classLabels, test_size=.30)
 
     ## BASELINE ##
-
+    print("\n# Base line #\n")
     # Create a dummy classifier that predicts the most frequent class (majority class)
     dummy_classifier = DummyClassifier(strategy="most_frequent")
 
@@ -19,13 +19,21 @@ def run(X, df, classLabels, np, pd, plt):
 
     # Make predictions on the test data
     dummy_predictions = dummy_classifier.predict(data_test)
+    test_error_rate = np.sum(dummy_predictions!=label_test) / len(label_test)
+    # Number of miss-classifications
+    print('Error rate: {0} % out of {1}'.format(test_error_rate*100,len(label_test)))
 
     # Calculate accuracy to evaluate the baseline model
     baseline_accuracy = accuracy_score(label_test, dummy_predictions)
-
+    
     print("Baseline Model Accuracy: {:.2f}%".format(baseline_accuracy * 100))
 
+    plt.figure(2, figsize=(9,9))
+    plt.hist([label_train, label_test, dummy_predictions], color=['red','green','blue'], density=True)
+    plt.legend(['Training labels','Test labels','Estimated test labels'])
+
     ## LOGISTIC REGRESSION ##
+    print("\n# Logistic regression #\n")
     # Standardize the training and set set based on training set mean and std
     mu = np.mean(data_train, 0)
     sigma = np.std(data_train, 0)
@@ -48,8 +56,11 @@ def run(X, df, classLabels, np, pd, plt):
     # Calculate accuracy to evaluate the baseline model
     logreg_accuracy = accuracy_score(label_test, data_test_est)
 
-    print("logreg Model Accuracy: {:.2f}%".format(logreg_accuracy * 100))
+    print("Logistic regression Model Accuracy: {:.2f}%".format(logreg_accuracy * 100))
 
     plt.figure(2, figsize=(9,9))
     plt.hist([label_train, label_test, data_test_est], color=['red','green','blue'], density=True)
     plt.legend(['Training labels','Test labels','Estimated test labels'])
+
+    ## METHOD 2 ##
+    print("\n# Method 2 #\n")
